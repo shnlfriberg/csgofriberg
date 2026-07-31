57b35ac499ef643deee3e87ea5efa53a9707874b
Xinhai <62863922+xinhai-ai@users.noreply.github.com>
2026-07-31 18:44:30 +0800

    feat: add friend links

--- client/src/config/friendLinks.ts
+++ client/src/config/friendLinks.ts
@@ -6,4 +6,6 @@ export interface FriendLink {
 
 export const FRIEND_LINKS: FriendLink[] = [
   // { name: 'Example', href: 'https://example.com/', description: 'Site description' },
+  { name: '玩烂梗收集', href: 'https://sb6657.cn/', description: '66577直播间烂梗收集' },
+  { name: '弗一把学习网站', href: 'https://inkfriberg.online/', description: '群友制作的弗一把学习网站' },
 ];
