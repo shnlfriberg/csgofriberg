da0352477e1cf4607b70b37e2cd132328552eee1
Xinhai <62863922+xinhai-ai@users.noreply.github.com>
2026-08-01 11:52:10 +0800

    chore: friend link

--- client/src/config/friendLinks.ts
+++ client/src/config/friendLinks.ts
@@ -6,6 +6,6 @@ export interface FriendLink {
 
 export const FRIEND_LINKS: FriendLink[] = [
   // { name: 'Example', href: 'https://example.com/', description: 'Site description' },
-  { name: '玩烂梗收集', href: 'https://sb6657.cn/', description: '66577直播间烂梗收集' },
+  { name: 'sb6657.cn', href: 'https://sb6657.cn/', description: '6657玩机器烂梗收集' },
   { name: '弗一把学习网站', href: 'https://inkfriberg.online/', description: '群友制作的弗一把学习网站' },
 ];
